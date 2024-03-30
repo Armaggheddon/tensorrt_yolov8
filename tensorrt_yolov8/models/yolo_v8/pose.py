c1922976b3a5abc2a743553a7b54d62855f3d330
Armaggheddon <alebrum97@gmail.com>
2024-03-30 13:16:13 +0000

    Fixed imports and top_k, added draw_results with support functions to build list of start-end points for pose skeleton

--- tensorrt_yolov8/models/yolo_v8/pose.py
+++ tensorrt_yolov8/models/yolo_v8/pose.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
-from typing import List
+from typing import List, Tuple, Dict
 
-from core.models.base import ModelBase
-from core.models.types import ModelResult, PosePoint
+from tensorrt_yolov8.core.models.base import ModelBase
+from tensorrt_yolov8.core.models.types import ModelResult, PosePoint
 from .common import yolo_preprocess
 from .labels import POSE_KEYPOINT_LABELS
 
@@ -14,8 +14,8 @@ class Pose(ModelBase):
 
     def __init__(
             self,
-            input_shapes: list[tuple[int, int, int]],
-            output_shapes: list[tuple[int, int, int]],
+            input_shapes: List[tuple[int, int, int]],
+            output_shapes: List[tuple[int, int, int]],
     ):
         self.labels = POSE_KEYPOINT_LABELS
 
@@ -42,7 +42,7 @@ class Pose(ModelBase):
 
         m_outputs = m_outputs[:, m_outputs[4, :] > min_prob]
 
-        #class id is person for all keypoints
+        #class id is person for all kp
         class_id = 0
         scores = m_outputs[4, :]
 
@@ -58,17 +58,23 @@ class Pose(ModelBase):
             scores=scores.astype(float).tolist(),
             score_threshold=min_prob,
             nms_threshold=nms_score,
-            top_k=top_k
+            # top_k=top_k
         )
+
+        _take = min(top_k, len(indexes))
+        if _take != len(indexes):
+            indexes = indexes[:_take]
 
         results = []
 
         for index in indexes:
             if isinstance(index, list): index = index[0]
 
-            x, y, w, h = bboxes[:, index]
-            score, *keypoints = m_outputs[4:, index]
+            x, y, w , h = bboxes[:, index]
+            score, *kp = m_outputs[4:, index]
 
+            # TODO find better handling for case of custom labels
+            # for dictionary of keypoint labels keys
             results.append(
                 ModelResult(
                     model_type=Pose.model_type,
@@ -77,13 +83,13 @@ class Pose(ModelBase):
                     confidence=score,
                     x1=x,
                     y1=y,
-                    w=w,
+                    w=w,   
                     h=h,
                     keypoints=[
                         PosePoint(
-                            x=int(keypoints[i*3] * self.src_img_w / self.input_shape[2]),
-                            y=int(keypoints[i*3+1] * self.src_img_h / self.input_shape[3]),
-                            v=keypoints[i*3+2],
+                            x=int(kp[i*3] * self.src_img_w / self.input_shape[2]),
+                            y=int(kp[i*3+1] * self.src_img_h / self.input_shape[3]),
+                            v=kp[i*3+2],
                             label=POSE_KEYPOINT_LABELS[i]
                         )
                         for i in range(0, len(POSE_KEYPOINT_LABELS))
@@ -92,3 +98,90 @@ class Pose(ModelBase):
             )
 
         return results
+    
+    # Can only work with the default YoloV8 POSE model
+    # since in general the keypoint labels or what they represent
+    # cannot be known
+    def get_lines(self, kp: List[PosePoint], **kwargs) -> List[Tuple[int, int, int, int]]:
+        
+        """
+        POSE_KEYPOINT_LABELS = [
+            0 = "nose",
+            1 = "left-eye",
+            2 = "right-eye",
+            3 = "left-ear",
+            4 = "right-ear",
+
+            5 = "left-shoulder",
+            6 = "right-shoulder",
+            7 = "left-elbow",
+            8 = "right-elbow",
+            9 = "left-wrist",
+            10 = "right-wrist",
+            11 = "left-hip",
+            12 = "right-hip",            
+            13 = "left-knee",
+            14 = "right-knee",
+            15 = "left-ankle",
+            16 = "right-ankle",
+        ]
+        """
+
+        lines = [
+            # face
+            (kp[4].x, kp[4].y, kp[2].x, kp[2].y),
+            (kp[2].x, kp[2].y, kp[0].x, kp[0].y),
+            (kp[0].x, kp[0].y, kp[1].x, kp[1].y),
+            (kp[1].x, kp[1].y, kp[3].x, kp[3].y),
+            # right side
+            (kp[10].x, kp[10].y, kp[8].x, kp[8].y),
+            (kp[8].x, kp[8].y, kp[6].x, kp[6].y),
+            (kp[6].x, kp[6].y, kp[12].x, kp[12].y),
+            (kp[12].x, kp[12].y, kp[14].x, kp[14].y),
+            (kp[14].x, kp[14].y, kp[16].x, kp[16].y),
+            # left side
+            (kp[9].x, kp[9].y, kp[7].x, kp[7].y),
+            (kp[7].x, kp[7].y, kp[5].x, kp[5].y),
+            (kp[5].x, kp[5].y, kp[11].x, kp[11].y),
+            (kp[11].x, kp[11].y, kp[13].x, kp[13].y),
+            (kp[13].x, kp[13].y, kp[15].x, kp[15].y),
+
+            # connect left and right hips and shoulders
+            (kp[12].x, kp[12].y, kp[11].x, kp[11].y),
+            (kp[6].x, kp[6].y, kp[5].x, kp[5].y),
+        ]
+        return lines
+    
+    def draw_results(self, image : np.ndarray, results : List[ModelResult], **kwargs) -> np.ndarray:
+        
+        img_overlay = image.copy()
+        
+        for result in results:
+            x, y, w, h = result.x1, result.y1, result.w, result.h
+
+            # Draw bbox
+            cv2.rectangle(img_overlay, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            # Draw label+confidence
+            cv2.putText(
+                img_overlay,
+                f"{result.class_label} {result.confidence:.2f}",
+                (x, y-10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 255, 0),
+                2
+            )
+
+            # Draw skeleton
+            lines = self.get_lines(result.keypoints)
+            for line in lines:
+                cv2.line(img_overlay, (line[0], line[1]), (line[2], line[3]), (0, 255, 0), 2)
+            
+            # Draw keypoints
+            for point in result.keypoints:
+                if point.v > 0:
+                    cv2.circle(img_overlay, (point.x, point.y), 5, (0, 255, 0), -1)
+            
+
+        return img_overlay
