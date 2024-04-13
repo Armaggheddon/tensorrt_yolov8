c1b8461490440739e2b574f454f57ae5cc183653
Armaggheddon <alebrum97@gmail.com>
2024-04-13 20:05:03 +0000

    Updated model base protocol to support batch processing

--- tensorrt_yolov8/core/models/base.py
+++ tensorrt_yolov8/core/models/base.py
@@ -9,14 +9,47 @@ class ModelBase(Protocol):
     input_shapes: List[Tuple[int, int, int]]
     output_shapes: List[Tuple[int, int, int]]
     
-    def preprocess(self, image: np.ndarray, **kwargs) -> np.ndarray:
-        """Protocol hint"""
+    def preprocess(self, images: List[np.ndarray], **kwargs) -> List[np.ndarray]:
+        """
+        Handles preprocessing of the input images based on the model requirements.
+
+        Arguments:
+        - images -- list of images to preprocess
+        - kwargs -- additional arguments to pass to the preprocessing function
+
+        Returns:
+        - preprocessed_images -- A list where each item is the preprocessed image/s as a 1D contiguous array.
+            If the model has multiple inputs, then each item represents that input tensor that will be fed to the model.
+        """
         ...
     
-    def postprocess(self, output: np.ndarray, min_prob: float, top_k: int, **kwargs) -> Any:
-        """Protocol hint"""
+    def postprocess(self, output: List[np.ndarray], min_prob: float, top_k: int, **kwargs) -> List[List[ModelResult]]:
+        """
+        Handles postprocessing of the model output. 
+
+        Arguments:
+        - output -- The list of the model outputs where each item is a 1D contiguous array
+        - min_prob -- the minimum confidence threshold to consider a detection
+        - top_k -- the maximum number of detections to keep
+        - kwargs -- additional arguments to pass to the post
+
+        Returns:
+        - results -- A list where each item is a list of ModelResult objects represents the 
+            detections for each batch, e.g. results[0] are the detections for the first image given.
+        """
         ...
 
-    def draw_results(self, image: np.ndarray, results: List[ModelResult], **kwargs) -> np.ndarray:
-        """Protocol hint"""
+    def draw_results(self, image: List[np.ndarray], results: List[List[ModelResult]], **kwargs) -> List[np.ndarray]:
+        """
+        Utility function that draws the results on the given images based on the results obtained.
+
+        Arguments:
+        - images -- list of images to draw the results on, if is a single image, then it should be a list with a single item
+        - results -- list of lists of ModelResult objects representing the detections for each image/images (in case of batched input).
+            It is the one that the user gets from the postprocess method.
+        - kwargs -- additional arguments to pass to the drawing function
+
+        Returns:
+        - images -- A list of images with the results drawn on them. The original images are not modified.
+        """
         ...
