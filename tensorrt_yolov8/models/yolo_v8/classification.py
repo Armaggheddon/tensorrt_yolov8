daf9630cf8104c3ffe270f667cae366b7233dbbd
Armaggheddon <alebrum97@gmail.com>
2024-04-13 20:08:02 +0000

    Added batch support

--- tensorrt_yolov8/models/yolo_v8/classification.py
+++ tensorrt_yolov8/models/yolo_v8/classification.py
@@ -26,42 +26,64 @@ class Classification(ModelBase):
         self.input_shape = input_shapes[0]
         self.output_shape = output_shapes[0]
 
-    def preprocess(self, image : np.ndarray, **kwargs) -> np.ndarray:
-        return yolo_preprocess(image, to_shape=self.input_shape, swap_rb=True)
+    def preprocess(self, images: List[np.ndarray], **kwargs) -> List[np.ndarray]:
+        return [yolo_preprocess(images, to_shape=self.input_shape, swap_rb=True)]
 
-
-    def postprocess(self, output: np.ndarray, min_prob: float, top_k: int, **kwargs) -> List[ModelResult]:
-
-        m_outputs = np.reshape(output[0], self.output_shape)
-
-        if m_outputs.shape[0] > 1:
-            # output is batched
-            pass
-
-        # TODO: see detection.py
-        assert m_outputs.shape[0] == 1
-
-        m_outputs = m_outputs[0]
-
-        class_ids = np.argpartition(m_outputs, -top_k)[-top_k:]
-        class_ids = class_ids[np.argsort(m_outputs[class_ids])][::-1]
-        class_ids = class_ids[m_outputs[class_ids] > min_prob]
+    def __postprocess_batch(self, output: np.ndarray, batch_id: int, min_prob: float, top_k: int, **kwargs) -> List[ModelResult]:
+        
+        class_ids = np.argpartition(output, -top_k)[-top_k:]
+        class_ids = class_ids[np.argsort(output[class_ids])][::-1]
+        class_ids = class_ids[output[class_ids] > min_prob]
 
         results = [ 
             ModelResult(
                 model_type=Classification.model_type, 
                 label_id=idx,
                 label_name=CLASSIFICATION_LABELS[idx],
-                confidence=m_outputs[idx]
+                confidence=output[idx]
             ) for idx in class_ids
         ]
         
         return results
+
+    def postprocess(self, output: List[np.ndarray], min_prob: float, top_k: int, **kwargs) -> List[List[ModelResult]]:
+
+        m_outputs = np.reshape(output[0], self.output_shape)
+
+        results = []
+
+        for batch_id in range(m_outputs.shape[0]):
+            results.append(
+                self.__postprocess_batch(m_outputs[batch_id, :], batch_id, min_prob, top_k, **kwargs)
+            )
+
+        return results
     
-    def draw_results(self, image : np.ndarray, results : List[ModelResult], **kwargs) -> np.ndarray:
+
+    def __draw_result(self, image: np.ndarray, result: List[ModelResult], **kwargs) -> np.ndarray:
+
+        img_overlay = image.copy()
+        for res in result:
+            if res.model_type != Classification.model_type: continue
+
+            cv2.putText(
+                img_overlay, 
+                f"{res.label_name} @ {res.confidence*100:.1f}%", 
+                (10, 30), 
+                cv2.FONT_HERSHEY_SIMPLEX, 
+                1, 
+                (0, 0, 255), 
+                2
+            )
+
+        return img_overlay
+
+
+    def draw_results(self, images: List[np.ndarray], results: List[List[ModelResult]], **kwargs) -> List[np.ndarray]:
         
-        for res in results:
-            label = f"{res.label_name} @ {res.confidence*100:.1f}%"
-            cv2.putText(image, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        imgs_overlay = []
+
+        for batch_id, batch in enumerate(results):
+            imgs_overlay.append(self.__draw_result(images[batch_id], batch, **kwargs))
         
-        return image
+        return imgs_overlay
--- tensorrt_yolov8/models/yolo_v8/common.py
+++ tensorrt_yolov8/models/yolo_v8/common.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 def yolo_preprocess(
-        *images : np.ndarray, 
+        images : List[np.ndarray], 
         to_shape : np.ndarray,
         swap_rb : bool = False) -> np.ndarray:
     """
@@ -19,16 +19,18 @@ def yolo_preprocess(
             raise ValueError(f"The number of images provided is too large: got {len(images)} images, expected at most {to_shape[0]} images.")
     
     # resize to shape x, y
-    preproc_images = [cv2.resize(img, (to_shape[2], to_shape[3])).astype(np.float32) for img in images]
-    preproc_images = np.array(preproc_images, dtype=np.float32)
+    preproc_images = [cv2.resize(img, (to_shape[2], to_shape[3])) for img in images]
+    preproc_images = np.stack(preproc_images, axis=0).astype(np.float32)
+
     if swap_rb:
         preproc_images = preproc_images[:, :, :, ::-1] # BGR to RGB
+
     preproc_images /= 255.0
     preproc_images = preproc_images.transpose((0, 3, 1, 2)) # BHWC to BCHW
-    
+
     # Pad batch with zeros if necessary
     if preproc_images.shape[0] < to_shape[0]:
-        preproc_images = np.concatenate([preproc_images, np.zeros((to_shape[0] - preproc_images.shape[0], *preproc_images.shape[1:]), dtype=np.float32)], axis=0)
-    
-    preproc_images = np.ascontiguousarray(np.asarray(preproc_images)).ravel()
-    return preproc_images
+        #preproc_images = np.stack([preproc_images, np.zeros((to_shape[0] - preproc_images.shape[0], *preproc_images.shape[1:]), dtype=np.float32)], axis=0)
+        preproc_images = np.stack([preproc_images, np.zeros((to_shape[0] - preproc_images.shape[0], *preproc_images.shape[1:]), dtype=np.float32)], axis=0)
+ 
+    return np.ascontiguousarray(preproc_images.ravel())
