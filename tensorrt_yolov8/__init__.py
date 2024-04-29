9860461bd1c8f23486eb42f4878f6ed8244f6d0b
Armaggheddon <alebrum97@gmail.com>
2024-04-29 16:34:22 +0000

    Added dynamic batches handling and parameter checks on initialization

--- tensorrt_yolov8/__init__.py
+++ tensorrt_yolov8/__init__.py
@@ -26,6 +26,7 @@ _model_types = [
     "pose",
     "segmentation",
     "obb",
+    "custom"
 ]
 
 
@@ -41,15 +42,28 @@ class Pipeline():
             warmup: bool = False,
             custom_model: ModelBase = None
     ):
+        if model_type not in _model_types:
+            raise ValueError(f"Model type must be one of {_model_types}")
+        if not os.path.exists(model_path):
+            raise FileNotFoundError(f"Model path {model_path} does not exist")
+        
+        if not model_path.endswith(".engine"):
+            raise ValueError("Only TensorRT engines are supported. Use trtexec or the library's tensorrt_yolov8.engine_utils.engine_builder to build the engine from an ONNX model file")
+        
         
         self.__engine = EngineHelper(model_path)
-
-        self.__model: ModelBase = getattr(
-            importlib.import_module(f"tensorrt_yolov8.models.yolo_v8.{model_type}"), model_type.capitalize()
-        )(
-            input_shapes=self.__engine.input_shapes, 
-            output_shapes=self.__engine.output_shapes
-        )
+        
+        if model_type is "custom":
+            # if custom_model is None:
+            #     raise ValueError("Custom model must be provided")
+            raise NotImplementedError("Custom model not implemented yet")
+        else:
+            self.__model: ModelBase = getattr(
+                importlib.import_module(f"tensorrt_yolov8.models.yolo_v8.{model_type}"), model_type.capitalize()
+            )(
+                input_shapes=self.__engine.input_shapes, 
+                output_shapes=self.__engine.output_shapes
+            )
 
     def __call__(
             self,
@@ -63,8 +77,9 @@ class Pipeline():
         if not is_list:
             images = [images]
 
+        batch_size = len(images)
         preprocess: List[np.ndarray] = self.__model.preprocess(images)
-        predictions = self.__engine.infer(preprocess)
+        predictions = self.__engine.infer(preprocess, batch_size)
         postprocessed = self.__model.postprocess(predictions, min_prob, top_k, **kwargs)
 
         return postprocessed if is_list else postprocessed[0]
