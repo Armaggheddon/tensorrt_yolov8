e6eedc7e766afffec5f2a31789714c4fc32f917a
Armaggheddon <alebrum97@gmail.com>
2024-04-29 16:33:36 +0000

    Example for dynamic batches

--- examples/ex_detection.py
+++ examples/ex_detection.py
@@ -4,8 +4,8 @@ from tensorrt_yolov8 import Pipeline
 
 if __name__ == "__main__":
 
-    model_path = "/home/Documents/Experiments/TENSORRT/tensorrt_yolov8/examples/yolov8s_det_b1_fp16.engine"
-    model_path = "/home/Documents/Experiments/TENSORRT/tensorrt_yolov8/examples/m_export_yolov8s_fp16_dynamic.onnx"
+    # model_path = "/home/Documents/Experiments/TENSORRT/tensorrt_yolov8/examples/yolov8s_det_b1_fp16.engine"
+    model_path = "/home/Documents/Experiments/TENSORRT/tensorrt_yolov8/examples/m_export_yolov8s_fp16_dynamic.engine"
     # model_path = "/home/Documents/Experiments/TENSORRT/tensorrt_yolov8/examples/v8s_b4_fp32.engine"
     image_path = "/home/Documents/Experiments/TENSORRT/tensorrt_yolov8/examples/demo_img.jpg"
     image = cv2.imread(image_path)
@@ -16,4 +16,4 @@ if __name__ == "__main__":
     
     img_result = det_pipe.draw_results(image, results)
 
-    cv2.imwrite(f"{image_path.split('.jpg')[0]}_det.jpg", img_result)
+    cv2.imwrite(f"{image_path.split('.jpg')[0]}_det.jpg", img_result[0])
